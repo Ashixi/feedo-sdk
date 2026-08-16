07ef189497a0af61382188a9eb4092729f3ac5ba
Andrii Shumko <andriishumko@gmail.com>
2026-08-16 22:09:45 +0200

    chore: bump version to 0.1.20

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="feedo-sdk",
-    version="0.1.19",
+    version="0.1.20",
     description="The official Developer SDK for Feedo Protocol",
     long_description=long_description,
     long_description_content_type="text/markdown",
